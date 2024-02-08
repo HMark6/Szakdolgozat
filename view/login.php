c49abccbeffbe7dcfa4a15776a873ce6e0ee19ee
HMark6 <hadnagymark2004@gmail.com>
2024-02-08 12:40:30 +0100

    login.php létrehozása
    létrehoztam egy kezdetleges bejelentkezési felületet

---
+++ view/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/Szakdolgozat/css/login.css" rel="stylesheet">
+    <title>Regisztráció</title>
+</head>
+<body>
+    <div class="container">
+        <div class="box form-box">
+            <header>Bejelentkezés</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="email">E-mail</label>
+                    <input type="text" name="email" id="email" required>
+                </div>
+                <div class="field input">
+                    <label for="password">Jelszó</label>
+                    <input type="text" name="password" id="password" required>
+                </div>
+                <div class="field">
+                    <input type="submit" name="submit" value="Bejelentkezés" required>
+                </div>
+                <div class="links">
+                    Nincs még fiókod? <a href="register.html">Regisztrálás</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+
