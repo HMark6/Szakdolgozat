fb42c3552eb15c7b8eae8bf282babf783a40d61f
HMark6 <hadnagymark2004@gmail.com>
2024-02-08 12:03:40 +0100

    menu.php javítása
    A menu.php-ban a nav bar hibáit kijavítottam, hogy megjelenítse a navigációs pontokat

--- view/menu.php
+++ view/menu.php
@@ -9,20 +9,30 @@
 </head>
 <body>
 
+<?php
+$logged_in = false;
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Étlap</a>
+    <a class="navbar-brand">Étlap</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="index.html">Főoldal</a>
+                <a class="nav-link" href="\Szakdolgozat\index.php">Menü</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="menu.html">Étlap</a>
+            <li class="nav-item">
+                <a class="nav-link" href="\Szakdolgozat\view\register.php">Regisztráció/Bejelentkezés</a>
             </li>
+            <?php if ($logged_in) : ?>
+                <!-- Ha bejelentkezett a felhasználó -->
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profil</a>
+                </li>
+            <?php endif; ?>
         </ul>
     </div>
 </nav>
