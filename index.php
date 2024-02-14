10bbfed2da12ac40229de5ae4a0fa6942a182d33
HMark6 <hadnagymark2004@gmail.com>
2024-02-14 12:50:32 +0100

    index.php.javítás
    Átállítottam a linkeket és kibővítettem a nav bar-t

--- index.php
+++ index.php
@@ -12,11 +12,13 @@
 <body>
 
 <?php
-$logged_in = false;
+session_start();
+
+$logged_in = isset($_SESSION['user_id']); // Ellenőrizzük, hogy a felhasználó be van-e jelentkezve
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand">Menü</a>
+    <a class="navbar-brand" href="#">Menü</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -24,20 +26,30 @@ $logged_in = false;
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="view\menu.php">Étlap</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="\Szakdolgozat\view\login.php">Bejelentkezés/Regisztráció</a>
+                <a class="nav-link" href="view/menu.php">Étlap</a>
             </li>
             <?php if ($logged_in) : ?>
                 <!-- Ha bejelentkezett a felhasználó -->
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Profil</a>
+                    <a class="nav-link" href="view\user_dashboard.php">Profil</a>
+                </li>
+            <?php endif; ?>
+            <!-- Ha a felhasználó be van jelentkezve, akkor a Kijelentkezés link jelenjen meg -->
+            <?php if ($logged_in) : ?>
+                <!-- Ha a felhasználó be van jelentkezve, akkor megjelenítjük a Kijelentkezés lehetőséget -->
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Kijelentkezés</a>
+                </li>
+            <?php else: ?>
+                <!-- Ha a felhasználó nincs bejelentkezve, akkor megjelenítjük a Bejelentkezés/Regisztráció lehetőséget -->
+                <li class="nav-item">
+                    <a class="nav-link" href="\Szakdolgozat\view\login.php">Bejelentkezés/Regisztráció</a>
                 </li>
             <?php endif; ?>
         </ul>
     </div>
 </nav>
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
